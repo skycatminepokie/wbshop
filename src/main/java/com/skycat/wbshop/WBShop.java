8775f9efd833c2881dc169a81fc2aeb17b95a6aa
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2023-09-29 10:09:01 -0700

    Shell of commands

--- src/main/java/com/skycat/wbshop/WBShop.java
+++ src/main/java/com/skycat/wbshop/WBShop.java
@@ -1,25 +1,42 @@
 package com.skycat.wbshop;
 
+import com.skycat.wbshop.command.CommandHandler;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.world.ServerWorld;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class WBShop implements ModInitializer, ServerWorldEvents.Load {
+public class WBShop implements ModInitializer, ServerWorldEvents.Load, ServerWorldEvents.Unload {
     public static final String MOD_ID = "wbshop";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-    public static WBShopEconomy ECONOMY;
+    /**
+     * Null when world is remote or not loaded.
+     */
+    public static @Nullable WBShopEconomy ECONOMY = null;
+    private static final CommandHandler COMMAND_HANDLER = new CommandHandler();
 
     @Override
     public void onInitialize() {
         ServerWorldEvents.LOAD.register(this);
+        CommandRegistrationCallback.EVENT.register(COMMAND_HANDLER);
     }
 
     @Override
     public void onWorldLoad(MinecraftServer server, ServerWorld world) {
-        if (world.isClient) return;
+        if (world.isClient) {
+            ECONOMY = null; // Potentially unnecessary? Not gonna mess with it though.
+            return;
+        }
         ECONOMY = server.getOverworld().getPersistentStateManager().getOrCreate(WBShopEconomy::readFromNbt, WBShopEconomy::new, WBShopEconomy.SAVE_ID);
+    }
+
+
+    @Override
+    public void onWorldUnload(MinecraftServer server, ServerWorld world) {
+        ECONOMY = null;
     }
 }
--- src/main/java/com/skycat/wbshop/command/CommandHandler.java
+++ src/main/java/com/skycat/wbshop/command/CommandHandler.java
@@ -1,4 +1,63 @@
 package com.skycat.wbshop.command;
 
-public class CommandHandler {
+import com.mojang.brigadier.CommandDispatcher;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.command.CommandRegistryAccess;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+
+import static net.minecraft.server.command.CommandManager.literal;
+
+
+public class CommandHandler implements CommandRegistrationCallback {
+    @Override
+    public void register(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess registryAccess, CommandManager.RegistrationEnvironment environment) {
+        // Sorted code in a depth-first thing for the tree
+        var root = dispatcher.getRoot();
+        var wbshop = literal("wbshop")
+                .build(); // TODO
+        var econ = literal("econ")
+                .build(); // TODO
+        var econGet = literal("get")
+                .build(); // TODO
+        var players = literal("players")
+                .build(); // TODO, also should be arg
+        var econAdd = literal("add")
+                .build(); // TODO
+        var playersPoints = literal("players")
+                .build(); // TODO, also should be arg
+        var points = literal("points")
+                .build(); // TODO, also should be arg
+        var econRemove = literal("remove")
+                .build(); // TODO
+        var econTotal = literal("total")
+                .build(); // TODO
+        var bal = literal("bal")
+                .build(); // TODO
+        var donate = literal("donate")
+                .build(); // TODO
+        var withdraw = literal("withdraw")
+                .build(); // TODO
+        var withdrawAll = literal("all")
+                .build(); // TODO
+
+        // Linking, depth-first
+        root.addChild(wbshop);
+            wbshop.addChild(econ);
+                econ.addChild(econGet);
+                    econGet.addChild(players);
+                econ.addChild(econAdd);
+                    econAdd.addChild(playersPoints);
+                        playersPoints.addChild(points);
+                econ.addChild(econRemove);
+                    econRemove.addChild(playersPoints);
+                econ.addChild(econTotal);
+            // TODO: Admin commands
+        root.addChild(bal);
+        root.addChild(donate);
+        root.addChild(withdraw);
+            withdraw.addChild(points);
+            withdraw.addChild(withdrawAll);
+
+    }
 }
