e58786e88415af05f0b2e03f033a1c6eb9a3bee2
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2023-12-15 19:34:36 -0800

    Fix some issues

--- src/main/java/com/skycat/wbshop/WBShop.java
+++ src/main/java/com/skycat/wbshop/WBShop.java
@@ -19,6 +19,8 @@ import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Objects;
+
 public class WBShop implements ModInitializer, ServerWorldEvents.Load, ServerWorldEvents.Unload, ServerLivingEntityEvents.AfterDeath {
     public static final String MOD_ID = "wbshop";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
@@ -41,8 +43,8 @@ public class WBShop implements ModInitializer, ServerWorldEvents.Load, ServerWor
      * @param server The server to grab the economy from.
      * @return The economy.
      */
-    public static Economy getEconomy(@NonNull MinecraftServer server) {
-        return server.getOverworld().getPersistentStateManager().getOrCreate(Economy::readFromNbt, Economy::new, Economy.SAVE_ID);
+    public static @NonNull Economy getEconomy(@NonNull MinecraftServer server) {
+        return Objects.requireNonNull(server.getOverworld().getPersistentStateManager().getOrCreate(Economy::readFromNbt, Economy::new, Economy.SAVE_ID)); // I'm pretty confident in that requireNonNull.
     }
 
     /**
@@ -50,6 +52,12 @@ public class WBShop implements ModInitializer, ServerWorldEvents.Load, ServerWor
      */
     public static @Nullable MinecraftServer getServer() {
         return server;
+    }
+
+    public static @NonNull Economy getEconomy(@NonNull ServerPlayerEntity player) {
+        MinecraftServer server = player.getServer();
+        assert server != null;
+        return getEconomy(server);
     }
 
     public static void updateBorder() throws BadStateException {
@@ -64,13 +72,11 @@ public class WBShop implements ModInitializer, ServerWorldEvents.Load, ServerWor
     @Override
     public void afterDeath(LivingEntity entity, DamageSource damageSource) {
         if (entity instanceof ServerPlayerEntity player) {
-            Economy econ = getEconomy();
-            if (econ != null) {
-                Account account = econ.getOrCreateAccount(player);
-                long pointsLost = (long) Math.ceil(account.balance() * 0.1);
-                account.removeBalance(pointsLost);
-                player.sendMessage(Text.of("You died and lost " + pointsLost + " points."));
-            }
+            Economy econ = getEconomy(player);
+            Account account = econ.getOrCreateAccount(player);
+            long pointsLost = (long) Math.ceil(account.balance() * 0.1);
+            account.removeBalance(pointsLost);
+            player.sendMessage(Text.of("You died and lost " + pointsLost + " points."));
         }
     }
 
@@ -79,7 +85,6 @@ public class WBShop implements ModInitializer, ServerWorldEvents.Load, ServerWor
         ServerWorldEvents.LOAD.register(this);
         CommandRegistrationCallback.EVENT.register(COMMAND_HANDLER);
         ServerLivingEntityEvents.AFTER_DEATH.register(this);
-        updateBorder();
         // CommonEconomy.register(EconomyProvider) // Hmm. How is this gonna work?
     }
 
@@ -89,6 +94,11 @@ public class WBShop implements ModInitializer, ServerWorldEvents.Load, ServerWor
             return;
         }
         WBShop.server = server;
+        try {
+            updateBorder();
+        } catch (BadStateException e) {
+            throw new RuntimeException(e); // State management shouldn't allow this. Probably.
+        }
     }
 
 
